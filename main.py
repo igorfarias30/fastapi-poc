8c112805364f889d765a3b73400856c33c051fdd
Igor Farias Sousa <igor_sousa@atlantico.com.br>
2020-02-20 15:53:09 -0300

    Execute with only python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from todo import router as todo_router
+import uvicorn
 
 app = FastAPI(
         title="Test FastAPI",
@@ -15,3 +16,7 @@ def main():
     return {"hello": "world"}
 
 app.include_router(todo_router, prefix='/todo', tags=["todo"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=80, log_level ="info")
